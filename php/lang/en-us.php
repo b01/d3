e634a4748c420bf6cf1d78c75e3ddfbd097f1371
Khalifah Shabazz <shabazzk@gmail.com>
2014-06-07 13:24:16 -0400

    Refactor all source and UI files.

--- php/lang/en-us.php
+++
@@ -1,7 +0,0 @@
-<?php namespace kshabazz\d3a\Lang;
-class Lang
-{
-	const
-		GENERAL_ERROR = "An application error has occured. Please try again later",
-		SQL_SAVE_ERROR = "Failed to save data; Which could mean lots of request to battle.net for the same data. Alerting system admin. You don't need to worry about this though";
-}
