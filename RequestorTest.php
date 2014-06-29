58bd354fe616f8fc3583821abf839fcb772fa35c
Khalifah Shabazz <shabazzk@gmail.com>
2014-06-29 14:26:34 -0400

    - Switched from cURL to guzzle for all HTTP request. - Fixed various unit test errors. - Changed PHP-VCR to use steam_wrapper, no longer needs cURL. - Removed more references to Twig template engine. - Changed isWeapon to take type array instead of an Item model.

--- RequestorTest.php
+++ RequestorTest.php
@@ -70,7 +70,7 @@ class BattleNet_RequestorTest extends \PHPUnit_Framework_TestCase
 	public function test_getting_a_valid_item()
 	{
 		$bnr = new \kshabazz\d3a\BattleNet_Requestor( $this->battleNetId );
-		$itemJson = $bnr->getItem( 'COGHsoAIEgcIBBXIGEoRHYQRdRUdnWyzFB2qXu51MA04kwNAAFAKYJMD' );
+		$itemJson = $bnr->getItem( 'item/COGHsoAIEgcIBBXIGEoRHYQRdRUdnWyzFB2qXu51MA04kwNAAFAKYJMD' );
 		$item = new \kshabazz\d3a\Model\Item( $itemJson );
 		$this->assertEquals( 'MightyWeapon1H_202', $item->id, 'Invalid item returned.' );
 	}
