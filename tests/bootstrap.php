ff5370d503edb9cfccd3d3978b051297282f848e
Khalifah Shabazz <shabazzk@gmail.com>
2014-07-04 23:18:07 -0400

    * Fixed issues with running unit test.

---
+++ tests/bootstrap.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Diablo 3 Assistant License is under The MIT License (MIT) [OSI Approved License]
+ * Please read LICENSE.txt, included with this software for the full licensing information.
+ * If no LICENSE.txt accompanied this software, then no license is granted.
+ * @package ${NAMESPACE}
+ *
+ * @copyright (c) 2012-2013 diablo-3-assistant by Khalifah K. Shabazz
+ * Timespamp: 11/7/13:8:07 AM
+ */
+require_once __DIR__ . '/../vendor/autoload.php';
+// configure PHP-VCR
+// specify which HTTP lib request to intercept.
+$vcrSettings = \VCR\VCR::configure()->enableLibraryHooks([ 'stream_wrapper' ]);
+define('FIXTURES_PATH', __DIR__ . DIRECTORY_SEPARATOR . 'fixtures' . DIRECTORY_SEPARATOR);
+\VCR\VCR::turnOn();
+?>
