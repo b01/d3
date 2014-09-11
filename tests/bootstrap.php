0ff6b47e1141d9346c800c85daf3a8718d6ff7d4
Khalifah Shabazz <shabazzk@gmail.com>
2014-09-10 21:03:17 -0400

    * Removed old license comment.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,12 +1,5 @@
 <?php
-/**
- * Diablo 3 Assistant License is under The MIT License (MIT) [OSI Approved License]
- * Please read LICENSE.txt, included with this software for the full licensing information.
- * If no LICENSE.txt accompanied this software, then no license is granted.
- * @package ${NAMESPACE}
- *
- * @copyright (c) 2012-2013 diablo-3-assistant by Khalifah K. Shabazz
- */
+// Load composer auto-loader.
 require_once __DIR__
 	. DIRECTORY_SEPARATOR . '..'
 	. DIRECTORY_SEPARATOR . 'vendor'
